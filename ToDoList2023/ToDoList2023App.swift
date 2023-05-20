86b4e90552cbeeeaceaf72123ccc4218c089ec44
veronicacheung <veronica.cheung1105@gmai.com>
2023-05-20 11:58:40 -0700

    Initial Commit

---
+++ ToDoList2023/ToDoList2023App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList2023App.swift
+//  ToDoList2023
+//
+//  Created by Veronica Cheung on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
